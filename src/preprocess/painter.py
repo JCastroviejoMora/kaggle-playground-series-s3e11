d8474ae3bf08fe69ba21a15e46a8c9c64e6ab2b6
JCastroviejoMora <jaume.castroviejo@gmail.com>
2023-05-05 21:16:40 +0200

    Introduced Sonarlint in painter.py

--- src/preprocess/painter.py
+++ src/preprocess/painter.py
@@ -1,8 +1,16 @@
+""" Script with functions to make visualizations"""
+import pandas as pd
 import plotly.express as px
 
 
-def make_scatter(df, x, y):
-
+def make_scatter(df: pd.DataFrame, x: str, y: str) -> int:
+    """
+    Make scatter plot from a dataframe and the columns x and y
+    :param df: pandas dataframe with data
+    :param x: column to plot over xaxis
+    :param y: column to plot over yaxis
+    :return: 1 if success
+    """
     fig = px.scatter(x=df[x], y=df[y])
 
     fig.update_layout(
@@ -13,19 +21,33 @@ def make_scatter(df, x, y):
     fig.write_html(f'../res/1_{x}_{y}.html')
     return 1
 
-def print_distribution(df, col):
+def print_distribution(df: pd.DataFrame, col: str) -> int:
+    """
+    Plot a distribution of a feature which is part of a dataframe
+    :param df: dataframe with the data
+    :param col: column to plot the distribution
+    :return: 1 if success
+    """
     fig = px.histogram(df, x=col, y=col,
                        marginal="box")
     fig.write_html(f'../res/2_distribution_{col}.html')
     return 1
 
-def print_correlation_matrix(df):
-
+def print_correlation_matrix(df: pd.DataFrame) -> int:
+    """
+    Print the heathmap with the correlation values
+    :param df: dataframe with all the data
+    :return: 1 if success
+    """
     fig = px.imshow(df.corr().round(2), text_auto=True)
     fig.write_html('../res/3_corr_matrix.html')
 
     return 1
 
-def clean_data(df):
-
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to clean data from a dataframe
+    :param df: dataframe to be cleaned
+    :return: Cleaned dataframe
+    """
     return df
